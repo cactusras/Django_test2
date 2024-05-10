9697635c1f6851ca9890df2536528f8c523cff08
syy88824 <jasmine.shyr@gmail.com>
2024-05-10 16:45:56 +0800

    Commit

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'db_project',
         'USER': 'root',
-        'PASSWORD': '1qazxsw2',
+        'PASSWORD': 'syy88824',
         'HOST': 'localhost',
         'PORT': '3306'
     }
