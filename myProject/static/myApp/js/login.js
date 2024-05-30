17e38a81df05bfa76f9f58adb18775fbf6ccb81c
cactusras <114577854+cactusras@users.noreply.github.com>
2024-05-30 13:09:03 +0800

    update_0530

--- myProject/static/myApp/js/login.js
+++ myProject/static/myApp/js/login.js
@@ -1,41 +1,43 @@
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
-    event.preventDefault(); // Prevent default form submission
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', async function(event) {
+        event.preventDefault(); // Prevent default form submission
 
-    const formData = new FormData(this); // Use 'this' to refer to the form element
- 
-    // Send the form data to the server
-    fetch('/login/', {
-        method: 'POST',
-        body: formData,
-        headers: {
-            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value // Ensure you include the CSRF token
-        }
-    })
-    .then(response => {
-        console.log('Response received:', response);
-        return response.json();
-    })
-    .then(data => {
-        print("to js")
-        if (data.status === 'success') {
-            if(data.message == 'client'){
-                // alert('Login success: ' + data.message);
-                window.location.href = '/home/'; // Redirect to the home page
-            }else if(data.message == 'clinic'){
-                // alert('Login success: ' + data.message);
-                window.location.href = '/clinic/home/'; // Redirect to the home page
-            }else if(data.message == 'doctor'){
-                // alert('Login success: ' + data.message);
-                window.location.href = '/doctor/page/'; // Redirect to the home page
-            }else{
-
+        const formData = new FormData(this); // Use 'this' to refer to the form element
+    
+        // Send the form data to the server
+        fetch('/login/', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value // Ensure you include the CSRF token
             }
-        } else {
-            alert('Login failed: ' + data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred. Please try again.');
+        })
+        .then(response => {
+            console.log('Response received:', response);
+            return response.json();
+        })
+        .then(data => {
+            // print("to js")
+            if (data.status === 'success') {
+                if(data.message == 'client'){
+                    // alert('Login success: ' + data.message);
+                    window.location.href = '/home/'; // Redirect to the home page
+                }else if(data.message == 'clinic'){
+                    // alert('Login success: ' + data.message);
+                    window.location.href = '/clinic/home/'; // Redirect to the home page
+                }else if(data.message == 'doctor'){
+                    // alert('Login success: ' + data.message);
+                    window.location.href = '/doctor/page/'; // Redirect to the home page
+                }else{
+
+                }
+            } else {
+                alert('Login failed: ' + data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again.');
+        });
     });
 });
